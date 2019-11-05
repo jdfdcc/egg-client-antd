5bf91528018787932f69f964a981b2bc5063b012
江道丰 <jiangdaofeng@iyunbao.com>
2019-11-05 14:26:10 +0800

    初始化项目

---
+++ config/defaultSettings.js
@@ -0,0 +1,15 @@
+export default {
+  "navTheme": "dark",
+  "primaryColor": "#1890FF",
+  "layout": "sidemenu",
+  "contentWidth": "Fluid",
+  "fixedHeader": false,
+  "autoHideHeader": false,
+  "fixSiderbar": true,
+  "menu": {
+    "locale": true
+  },
+  "title": "Ant Design Pro",
+  "pwa": false,
+  "iconfontUrl": ""
+};
