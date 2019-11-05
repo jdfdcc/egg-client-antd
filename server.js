775e3435a730c61c3637e9d931d0d8ca20273c96
江道丰 <jiangdaofeng@iyunbao.com>
2019-11-05 18:04:32 +0800

    修改服务7878

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app= express();
+
+// const resolve = file => path.resolve(__dirname, file);
+
+// const serve = (spath, cache) =>
+//   express.static(resolve(spath), {
+//     maxAge: 60 * 60 * 24 * 30,
+//   });
+
+// app.use(express.static(path.join(__dirname, '/static/')));
+// app.use('/static/', serve('./dist/static', true));
+
+// // app.engine('html', engines.lodash);
+// // app.engine('ejs', engines.ejs);
+// app.set('views', path.resolve(__dirname, './dist/'));
+// app.set('view engine', 'html');
+
+app.get('/client', (req, res) => {
+  res.sendFile(path.join(__dirname, '/dist/', 'index.html'));
+});
+
+app.listen(7878, () => {
+    console.log('Server is running at http://localhost:7878')
+})
